45aec666584e4a72b2233945f9eced5a3258df1b
burak <burak@BURAK>
2024-08-14 19:51:09 +0300

    Add project files.

---
+++ Employees.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PratikAbstraction
+{
+    public abstract class Employees
+    {
+        public virtual string EmployeeName { get; set; }
+        public virtual string EmployeeSurName { get; set; }
+
+        public virtual string Department { get; set; }
+
+        public Employees(string employeeName, string employeeSurName, string department)
+        {
+            EmployeeName = employeeName;
+            EmployeeSurName = employeeSurName;
+            Department = department;
+        }
+        public abstract void Task();
+
+    }
+
+   class SoftwareDeveloper : Employees
+    {
+        public SoftwareDeveloper(string employeeName, string employeeSurName, string department) : base(employeeName, employeeSurName, department)
+        { }
+
+        public override void Task()
+        {
+            Console.WriteLine("I work as a software developer!");
+        }
+    }
+
+    class SalesRepresentative : Employees
+    {
+        public SalesRepresentative(string employeeName, string employeeSurName, string department) : base(employeeName, employeeSurName, department)
+        {
+        }
+
+        public override void Task()
+        {
+            Console.WriteLine("Im a Sales Representative!");
+        }
+    }
+    class GraphicDesigner : Employees
+    {
+        public GraphicDesigner(string employeeName, string employeeSurName, string department) : base(employeeName, employeeSurName, department)
+        {}
+
+        public override void Task()
+        {
+            Console.WriteLine("Im a Graphic Designer!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using PratikAbstraction;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employees graphicdesigner = new GraphicDesigner("Zeynep", "Kırıcı", "Graphic");
+        Employees salesrepresentative = new SalesRepresentative("Burak", "Kırıcı", "Sales");
+        Employees softwaredeveloper = new SoftwareDeveloper("Emre", "Can", "Devoloper");
+
+        graphicdesigner.Task();
+        salesrepresentative.Task();
+        softwaredeveloper.Task();
+    }
+}
